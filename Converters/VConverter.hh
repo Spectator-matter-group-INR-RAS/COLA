937abe0e2996255133877df344a5ea457e63d938
alexsvetlichnyy <aleksandr.svetlichnyy@phystech.edu>
2022-12-20 16:23:32 +0300

    Subdirectory structure and virtual classes files

---
+++ Converters/VConverter.hh
@@ -0,0 +1,14 @@
+//
+// Created by alexsvetlichnyy on 20.12.22.
+//
+
+#ifndef GENERATORFRAMEWORK_VCONVERTER_HH
+#define GENERATORFRAMEWORK_VCONVERTER_HH
+
+
+class VConverter {
+
+};
+
+
+#endif //GENERATORFRAMEWORK_VCONVERTER_HH
---
+++ Generators/VGenerator.cc
@@ -0,0 +1,5 @@
+//
+// Created by alexsvetlichnyy on 20.12.22.
+//
+
+#include "VGenerator.hh"
