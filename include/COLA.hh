b03e423ab5b3cfa6ec380e170d263745a2cd88db
alexsvetlichnyy <aleksandr.svetlichnyy@phystech.edu>
2023-08-21 19:24:07 +0300

    variables for the parser delimeters

--- include/COLA.hh
+++ include/COLA.hh
@@ -217,12 +217,14 @@ namespace cola {
     inline cola::MetaData cola::MetaProcessor::parseStrToMeta(const std::string data) {
         std::vector<std::string> filters; 
         cola::MetaData metaData;
-        std::string trimedData = boost::trim_copy_if(data, boost::is_any_of("\n"));
-        boost::split(filters, trimedData, boost::is_any_of("\n"));
+        std::string fltSeparator = "\n111";
+        std::string nameSeparator = " ";
+        std::string trimedData = boost::trim_copy_if(data, boost::is_any_of(fltSeparator));
+        boost::split(filters, trimedData, boost::is_any_of(fltSeparator));
         for(int flt = 0; flt < filters.size(); ++flt){
             std::vector<std::string> tmp;
-            boost::split(tmp, filters.at(flt), boost::is_any_of(" "));
-            auto res = boost::trim_left_copy_if(filters.at(flt), boost::is_any_of(tmp.at(0)+" "));
+            boost::split(tmp, filters.at(flt), boost::is_any_of(nameSeparator));
+            auto res = boost::trim_left_copy_if(filters.at(flt), boost::is_any_of(tmp.at(0)+nameSeparator));
             metaData.filterParamMap.emplace(tmp.at(0), res);
             if (flt == 0) {
                 metaData.generatorName = tmp.at(0);
